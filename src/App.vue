2ba5da3b1f554e8646e2eb03d9cd6252248f9411
boris7ran <boris.ran7@gmail.com>
2019-07-15 20:34:20 +0200

    Zadatak 1

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <router-link to="/movies" class="navbar-brand">Movies</router-link> |
+      <router-link to="/about" class="navbar-brand">About</router-link>
+    </nav>
     <router-view/>
   </div>
 </template>
